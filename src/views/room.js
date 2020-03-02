bc47f95602c180e3adf2d85c75bd0e5fde33a810
hajczek <i.hajczewska@gmail.com>
2020-03-02 16:32:20 +0100

    Added code structure to room component

--- src/views/room.js
+++ src/views/room.js
@@ -1,14 +1,30 @@
 // room.js
 
 import $ from "jquery";
+import { header, footer } from "./index";
 
 export const room = () => {
   const fragment = $(new DocumentFragment());
 
-  const h1 = $(`<h1>Pokój</h1>`);
-  const slogan = $(`<p>Opis pokoju</p>`);
+  const box = $(`<div class="room-box"></div>`);
+  const title = $(`<h2 id="title">Pokój unarny</h2>`);
+  const details = $(`<p>Ilość łóżek: <span id="beds-quantity">1</span><br>
+  Ilość osób: <span id="persons-quantity">1</span><br>
+  Cena pokoju: <span id="room-price">80</span> zł<br>
+  Data przyjazdu: <input id="room-from" type="date"><br>
+  Data wyjazdu: <input id="room-to" type="date"><br>
+  Ilość pokoi: <input id="room-num" type="number" min="1" max="10"></p>`);
+  const button = $(`<button id="add-room">Wrzuć do kosza!</button>;`);
 
-  fragment.append(h1).append(slogan);
+  fragment
+    .append(header)
+    .append(box)
+    .append(footer);
+
+  box
+    .append(title)
+    .append(details)
+    .append(button);
 
   return fragment;
 };

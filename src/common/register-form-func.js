ef63a2bf69db0dbfdf901dac749a49b6d51a94cf
hajczek <i.hajczewska@gmail.com>
2020-03-05 10:43:22 +0100

    Added functionality for check if register user is in datatabse users

--- src/common/register-form-func.js
+++ src/common/register-form-func.js
@@ -37,39 +37,63 @@ export const activateRegisterForm = () => {
       let userEmail = $("#email").val();
       let userPassword = $("#pass").val();
 
-      // Save user login and password in database
-      // Connect with database and create a new user
-      fetch("http://localhost:3000/users", {
-        headers: { "Content-Type": "application/json; charset=utf-8" },
-        method: "POST",
-        body: JSON.stringify({
-          login: `${userEmail}`,
-          password: `${userPassword}`
-        })
-      });
+      // Connect with database
+      fetch("http://localhost:3000/users")
+        .then(response => response.json())
+        .then(users =>
+          // Filter users
+          users.filter(user => {
+            // Check, if login and pass are in database/users
+            if (user.login !== userEmail) {
+              // Save user login and password in database
+              // Connect with database and create a new user
+              fetch("http://localhost:3000/users", {
+                headers: { "Content-Type": "application/json; charset=utf-8" },
+                method: "POST",
+                body: JSON.stringify({
+                  login: `${userEmail}`,
+                  password: `${userPassword}`
+                })
+              });
 
-      // Clear input fields value
-      $("#email").val("");
-      $("#pass").val("");
-      $("#pass-repeat").val("");
+              // Clear input fields value
+              $("#email").val("");
+              $("#pass").val("");
+              $("#pass-repeat").val("");
 
-      // Clear fields for checking strength of password
-      $("span.weak").css("background-color", "#fff");
-      $("span.average").css("background-color", "#fff");
-      $("span.strong").css("background-color", "#fff");
+              // Clear fields for checking strength of password
+              $("span.weak").css("background-color", "#fff");
+              $("span.average").css("background-color", "#fff");
+              $("span.strong").css("background-color", "#fff");
 
-      // Hide form
-      $(".registration-box").css("display", "none");
+              // Hide form
+              $(".registration-box").css("display", "none");
 
-      // Display text after registration
-      $("main")
-        .find("header")
-        .after(afterRegister);
+              // Display text after registration
+              $("main")
+                .find("header")
+                .after(afterRegister);
+            } else if (user.login === userEmail) {
+              console.log("Taki użytkownik już istnieje w naszej bazie");
+
+              // Display info about different passwords
+              $("#info-err").html("Ten użytkownik jest już w naszej bazie.");
+              $("#info-err").css(
+                "background-color",
+                "rgba(255, 255, 255, 0.5)"
+              );
+
+              // Hide box with info after 3 minutes
+              setTimeout(() => {
+                $("#info-err").css("display", "none");
+              }, 3000);
+            }
+          })
+        )
+        .catch(error => console.log("Error ...", error));
     } else {
       // Display info about different passwords
-      $("#info-err").html(
-        "Hasła są różne. Wprowadź do obu pól takie same hasła."
-      );
+      $("#info-err").html("Hasła są różne. Wprowadź do obu pól to samo hasło.");
       $("#info-err").css("background-color", "rgba(255, 255, 255, 0.5)");
 
       // Hide box with info after 3 minutes

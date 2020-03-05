6795594ff20328464dcaa3c32fe14cb8891f8102
hajczek <i.hajczewska@gmail.com>
2020-03-05 11:23:06 +0100

    Remove functionality for check register password strength to file check-register-pass.js

---
+++ src/common/check-register-pass.js
@@ -0,0 +1,22 @@
+import $ from "jquery";
+
+export function checkRegisterPass() {
+  // Check strength of password
+  // Check if inputs are not empty
+  if ($("#email").val().length !== 0 && $("#pass").val().length !== 0) {
+    // Set color for weaknes password
+    $("span.weak").css("background-color", "#f504d5");
+  }
+
+  // Check if password has length 8 and 1 capital letter
+  if (/(?=.*?[A-Z])(?=.*?[a-z]).{8,}/.test($("#pass").val())) {
+    // If yes, set color for average password
+    $("span.average").css("background-color", "yellow");
+  }
+
+  // Check if password has length 12 and 1 capital letter and 1 digit
+  if (/(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{12,}/.test($("#pass").val())) {
+    // If yes, set color for strong password
+    $("span.strong").css("background-color", "greenyellow");
+  }
+}
--- src/common/register-form-func.js
+++ src/common/register-form-func.js
@@ -1,32 +1,12 @@
 import $ from "jquery";
+import { checkRegisterPass } from "./index";
 import { afterRegister } from "./../views/index";
 import { errorFunc } from "./index";
 
 export const activateRegisterForm = () => {
-  $("#email").change(registerUser);
-  $("#pass").change(registerUser);
+  $("#email").change(checkRegisterPass);
+  $("#pass").change(checkRegisterPass);
   $("#register-btn").on("click", sendForm);
-
-  function registerUser() {
-    // Check strength of password
-    // Check if inputs are not empty
-    if ($("#email").val().length !== 0 && $("#pass").val().length !== 0) {
-      // Set color for weaknes password
-      $("span.weak").css("background-color", "#f504d5");
-    }
-
-    // Check if password has length 8 and 1 capital letter
-    if (/(?=.*?[A-Z])(?=.*?[a-z]).{8,}/.test($("#pass").val())) {
-      // If yes, set color for average password
-      $("span.average").css("background-color", "yellow");
-    }
-
-    // Check if password has length 12 and 1 capital letter and 1 digit
-    if (/(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{12,}/.test($("#pass").val())) {
-      // If yes, set color for strong password
-      $("span.strong").css("background-color", "greenyellow");
-    }
-  }
 
   function sendForm(e) {
     if (

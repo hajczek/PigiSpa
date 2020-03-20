df0deddeb1b0a56cfa125b749abe7fba2622c595
hajczek <i.hajczewska@gmail.com>
2020-03-20 17:09:57 +0100

    Added functionality for display details about added element to cart after clicked button Wrzuc do kosza

--- src/cart/add-to-cart.js
+++ src/cart/add-to-cart.js
@@ -19,14 +19,40 @@ export const addToCart = () => {
   // Calculate the no. of days between two dates
   let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
 
+  $(`#slogan-basket`).css("display", "none");
+
   // Check if added product to cart is not a room
   if ($(`#room-from`).val() === undefined) {
     // Add to cookies details about added prodact to cart
     cart.add({
       name: $(`#title`).html(),
       count: $(`.num`).val(),
-      price: $(`.product-price`).html() * parseInt($(`.num`).val())
+      price: parseInt($(`.product-price`).html()) * parseInt($(`.num`).val())
     });
+
+    // Define li element with details about added product to display in cart
+    let cartLi = $(
+      `<li><span class="title">${$(
+        `#title`
+      ).html()}</span> x <span class="quant">${$(
+        `.num`
+      ).val()}</span><p class="value">Łączna wartość: <span class="price">${parseInt(
+        $(`.product-price`).html()
+      ) *
+        parseInt(
+          $(`.num`).val()
+        )}</span><br></p><span class="remove"></span></li>`
+    );
+
+    // Display cartLi element on page
+    $(`#basket-content`).append(cartLi);
+
+    // Define new whole value in cart after added element to cart
+    let allValue =
+      parseInt($(`#all-cost`).html()) +
+      parseInt($(`.product-price`).html()) * parseInt($(`.num`).val());
+    // Display new whole value of cart
+    $(`#all-cost`).html(allValue + " ZŁ");
   } else {
     // Add to cookies details about added prodact to cart
     cart.add({

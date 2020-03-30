e8ce4906965d10466533cf1b3e9fdc6b3b7d7a86
hajczek <i.hajczewska@gmail.com>
2020-03-30 10:06:00 +0200

    Changed port for databse for local

--- src/database/database-conntection.js
+++ src/database/database-conntection.js
@@ -1,5 +1,5 @@
 /** Path to database on Heroku json-server */
-export const url = "https://database-pigispa.herokuapp.com";
+// export const url = "https://database-pigispa.herokuapp.com";
 
 /** Path to databaseon local json-server */
-// export const url = "http://localhost:3004";
+export const url = "http://localhost:3004";
